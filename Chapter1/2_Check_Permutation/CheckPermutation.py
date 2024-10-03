ccbececc971776531ea5120d6798bf10ecc29962
daddyofadoggy <158696258+daddyofadoggy@users.noreply.github.com>
2024-10-03 03:56:11 -0700

    Update CheckPermutation.py

--- Chapter1/2_Check_Permutation/CheckPermutation.py
+++ Chapter1/2_Check_Permutation/CheckPermutation.py
@@ -3,5 +3,39 @@ from collection import Counter
 
 def check_permutation(str1, str2):
 	cnt = Counter()
-	
+	for ch in str1:
+		cnt[ch] +=1
+	for ch in str2:
+		if cnt[ch] == 0:
+			return False
+		else:
+			cnt[ch] -=1
+	return True
+
+
+class Test(unittest.TestCase):
+    dataT = (
+        ('abcd', 'bacd'),
+        ('3563476', '7334566'),
+        ('wef34f', 'wffe34'),
+    )
+    dataF = (
+        ('abcd', 'd2cba'),
+        ('2354', '1234'),
+        ('dcw4f', 'dcw5f'),
+    )
+
+    def test_cp(self):
+        # true check
+        for test_strings in self.dataT:
+            result = check_permutation(*test_strings)
+            self.assertTrue(result)
+        # false check
+        for test_strings in self.dataF:
+            result = check_permutation(*test_strings)
+            self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
 
